e29bae2f608a37f2ef596ab3cb7a909003bd8985
Sergey Kuznetsov <kuzzz99@gmail.com>
2026-08-16 12:33:48 +0100

    Add devshell

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "tasksd-client.nvim — Neovim client for tasksd";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          name = "tasksd-client.nvim";
+
+          packages = with pkgs; [
+            just
+            stylua
+            selene
+            lua-language-server
+            neovim
+          ];
+          shellHook = ''
+            echo "tasksd-client.nvim devshell — run 'just' to list recipes"
+          '';
+        };
+      });
+
+      formatter = forAllSystems (pkgs: pkgs.nixfmt);
+    };
+}
